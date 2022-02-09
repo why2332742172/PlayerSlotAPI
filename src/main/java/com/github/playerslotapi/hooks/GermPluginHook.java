1024d518bf19b710bc686c9457a0e60510517f09
why2332742172 <2332742172@qq.com>
2022-02-09 20:18:53 +0800

    add GermPluginHook:getItemFromSlot and setItemToSlot

--- src/main/java/com/github/playerslotapi/hooks/GermPluginHook.java
+++ src/main/java/com/github/playerslotapi/hooks/GermPluginHook.java
@@ -1,13 +1,35 @@
 package com.github.playerslotapi.hooks;
 
-import com.germ.germplugin.api.GermKeyAPI;
+import com.germ.germplugin.GermPlugin;
+import com.germ.germplugin.api.GermSlotAPI;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GermPluginHook {
 
-    public GermKeyAPI germKeyAPI = null;
+    public GermPlugin instance;
 
-    public GermPluginHook(){
-        this.germKeyAPI = new GermKeyAPI();
+    public GermPluginHook() {
+        this.instance = GermPlugin.getPlugin();
     }
 
+    public ItemStack getItemFromSlot(String identifier, Player player) {
+        List<String> list = new ArrayList<>();
+        list.add(identifier);
+        return GermSlotAPI.getGermSlotItemStacks(player, list).get(0);
+    }
+
+    public void setItemToSlot(String identifier, Player player, ItemStack toBePuttedItem, Boolean forceReplace) {
+        ItemStack itemInSlot = getItemFromSlot(identifier, player);
+
+        if (itemInSlot != null && itemInSlot.getType() != Material.AIR && !forceReplace) {
+            player.sendMessage("玩家" + player.getName() + "在槽位" + identifier + "已经有物品了!无法非强制替换!");
+            return;
+        }
+        GermSlotAPI.saveItemStackToIdentity(player, identifier, toBePuttedItem);
+    }
 }
